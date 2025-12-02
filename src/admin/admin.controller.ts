68fbfe022984162367990795e1163f499c6546a4
Jahzeal <jahzealibeh3@gmail.com>
2025-12-02 18:52:42 +0100

    adding dto

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,10 +1,18 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Patch,
+  UseGuards,
+} from '@nestjs/common';
 import { AdminService } from './admin.service';
 import { RolesGuard } from 'src/auth/guard/roles.guard';
 import { JwtGuard } from 'src/auth/guard';
 import { Roles } from 'src/auth/decorator/roles.decorator';
 import { GetUser } from 'src/auth/decorator/get-user.decorator';
 import type { User } from '@prisma/client';
+import { editUserDto } from 'src/users/dto/users.dto';
 
 @Controller('admin')
 @UseGuards(JwtGuard, RolesGuard) // attach the RolesGuard to all routes in this controller
@@ -14,5 +22,30 @@ export class AdminController {
   @Roles('ADMIN') // <-- Only admins can access
   getMe(@GetUser() user: User) {
     return user; // JWT payload
+  }
+
+  @Get('stats')
+  getAdminStats() {
+    return this.adminService.getStats();
+  }
+
+  @Get('users')
+  getAllUsers() {
+    return this.adminService.getAllUsers();
+  }
+
+  @Get('getAllProperties')
+  getAllProperties() {
+    return this.adminService.getAllProperties();
+  }
+
+  @Delete('deleteUser/:id')
+  deleteUser() {
+    return this.adminService.deleteUser('id');
+  }
+
+  @Patch('update-user/:id')
+  async editUser(@GetUser('id') userId: string, @Body() dto: editUserDto) {
+    return this.adminService.editUser(userId, dto);
   }
 }
--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,4 +1,50 @@
-import { Injectable } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { editUserDto } from '../users/dto/users.dto';
 
 @Injectable()
-export class AdminService {}
+export class AdminService {
+  constructor(private prisma: PrismaService) {}
+  async getStats() {
+    const totalUsers = await this.prisma.user.count();
+    const totalProperties = await this.prisma.property.count();
+    const rentalCount = await this.prisma.rental.count();
+    const bookingCount = await this.prisma.booking.count();
+    return {
+      totalUsers,
+      rentalCount,
+      bookingCount,
+      totalProperties,
+    };
+  }
+
+  async getAllUsers() {
+    return this.prisma.user.findMany();
+  }
+
+  async getAllProperties() {
+    return this.prisma.property.findMany();
+  }
+  async deleteUser(userId: string) {
+    return this.prisma.user.delete({
+      where: { id: userId },
+    });
+  }
+  async editUser(userId: string, dto: editUserDto) {
+    const data: any = { ...dto };
+    try {
+      return await this.prisma.user.update({
+        where: { id: userId },
+        data,
+      });
+    } catch (error) {
+      // Prisma throws P2025 if record doesn't exist
+      if (error.code === 'P2025') {
+        throw new NotFoundException('User not found');
+      }
+      throw error;
+    }
+  }
+}
