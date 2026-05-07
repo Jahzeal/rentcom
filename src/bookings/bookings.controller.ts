5d254ee83812242a689080ea73591d27a0d47512
Jahzeal <jahzealibeh3@gmail.com>
2026-05-07 12:32:47 +0100

    dates fixed

--- src/bookings/bookings.controller.ts
+++ src/bookings/bookings.controller.ts
@@ -13,11 +13,11 @@ import { GetUser } from 'src/auth/decorator/get-user.decorator';
 import { BookingsService } from './bookings.service';
 import { CreateBookingDto } from './dto/booking.dto';
 
-@UseGuards(JwtGuard, RolesGuard)
 @Controller('bookings')
 export class BookingsController {
   constructor(private bookingsService: BookingsService) {}
 
+  @UseGuards(JwtGuard, RolesGuard)
   @Post()
   createBooking(@GetUser('id') userId: string, @Body() dto: CreateBookingDto) {
     return this.bookingsService.createBooking(userId, dto);
@@ -33,12 +33,14 @@ export class BookingsController {
     return this.bookingsService.getReservedDates(id);
   }
 
+  @UseGuards(JwtGuard, RolesGuard)
   @Get('agent')
   @Roles('AGENT', 'ADMIN')
   getAgentBookings(@GetUser('id') userId: string) {
     return this.bookingsService.getAgentBookings(userId);
   }
 
+  @UseGuards(JwtGuard, RolesGuard)
   @Get('admin')
   @Roles('ADMIN')
   getAllBookings() {
